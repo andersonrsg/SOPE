8596edf2b71ca9547bcedea1f01ac8488d744630
Anderson Gralha <anderson.gralha@gmail.com>
2018-03-31 20:34:24 +0100

    iniciado signal

--- simgrep.c
+++ simgrep.c
@@ -16,6 +16,7 @@
 #include <regex.h>
 #include <dirent.h>
 #include <fcntl.h>
+#include <signal.h>
 
 #define MAXLEN  256
 #define BIT(n)  (0x01 << n)
@@ -34,6 +35,12 @@ struct Analysis {
     unsigned long matchesCount;
 
 };
+
+void sigint_handler(int signo)
+{
+    printf("Are you sure you want to terminate the program? (Y/N)");
+}
+
 
 // Assinaturas
 int main(int argc, char *argv[]);
@@ -60,6 +67,11 @@ int main(int argc, char *argv[]) {
     *token,
     *pattern = NULL,
     **files = NULL;
+
+    struct sigaction action;
+    action.sa_handler = sigint_handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
 
 
     /* Concatenate args */
@@ -249,8 +261,13 @@ int simgrep(char *pattern, char **filenames, unsigned char flags) {
             }
             else if(pid) {
                 // waitpid(pid, &r , WNOHANG);
+
             }
             else {
+                sigset_t mask;
+                sigfillset(&mask);
+                sigprocmask(SIG_SETMASK, &mask, NULL);
+                
                 dircontent = getDirContent(directories[i]);
 
                 if((dircontent != NULL) && (dircontent[0] != NULL)){
