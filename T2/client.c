a7e68632ab27626b65245d59af9330183330cb7f
Anderson Gralha <anderson.gralha@gmail.com>
2018-05-09 22:56:47 +0100

    finalmente bugfix

--- T2/client.c
+++ T2/client.c
@@ -6,12 +6,14 @@
 #include <sys/stat.h>
 #include <time.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 
 void postRequest(char *argv[], pid_t pid);
-void getResponse(int timeout);
+void getResponse(int timeout, pid_t pid);
 int readline(int fd, char *str);
-void parseResponse(char *response);
+void parseResponse(char *response, pid_t pid);
+void writeLog(pid_t pid, int reservedSeats, char *seats, char* id, char* error);
+
 
 int main(int argc, char *argv[]) {
     printf("** Running process %d (PGID %d) **\n", getpid(), getpgrp());
@@ -32,33 +34,29 @@ int main(int argc, char *argv[]) {
     }
     timeout = atoi(argv[1]);
     
-    pid = fork();
-    if (pid == -1) {
-        printf("Fatal error.");
-        exit(0);
-    } else if (pid == 0) {
-        //        sleep(2);
-        //        postRequest(argv, pid);
-        
-        getResponse(timeout);
-    } else {
-        //        getResponse(timeout);
-        
-        sleep(2);
-        postRequest(argv, pids);
-    }
+//    pid = fork();
+//    if (pid == -1) {
+//        printf("Fatal error.");
+//        exit(0);
+//    } else if (pid == 0) {
+//        //        sleep(2);
+//        //        postRequest(argv, pid);
+//
+//        getResponse(timeout, pids);
+//    } else {
+//        //        getResponse(timeout);
+//
+//        sleep(2);
+//        postRequest(argv, pids);
+//    }
     
     sleep(1);
     
     
+    char message[50] = "7 8 9 10 11";
+    char message2[50] = "29 99 101 102 9103";
     
-    
-    
-    // sleep(3);
-    // char word[256];
-    
-    // fgets(word, sizeof(word), stdin);
-    // write(fd,word,strlen(word));
+    writeLog(pids, 5, message, message2, "");
     
     
     
@@ -66,36 +64,89 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+/**
+ Parameters
+ pid: process pid
+ reservedSeats: The error id in case reservedSeats < 0 and the number of reserved seats in case reservedSeats > 0
+ seats: The string with all the reserved seats.
+ */
+void writeLog(pid_t pid, int reservedSeats, char *seats, char* id, char* error) {
+    
+    int fd;
+    char pidMessage[5];
+    char message[200];
+    int seatInt;
+    char *currSeat;
+    char *saveptr;
+    
+    if ((fd = open("clog.txt", O_WRONLY | O_APPEND | O_CREAT, 0644)) < 0) {
+        perror("Logfile");
+        exit(1);
+    }
+    
+    
+    
+    if (reservedSeats > 0) {
+        
+    currSeat = strtok(seats, " ");
+        
+        while (currSeat != NULL) {
+            snprintf(pidMessage, sizeof pidMessage, "%05d", pid);
+            
+            strcat(message, pidMessage);
+            strcat(message, " ");
+            strcat(message, currSeat);
+            strcat(message, ".");
+            write(fd, message, strlen(message));
+            snprintf(message, sizeof(message), "%d %s", reservedSeats, id);
+            strcat(message, "\n");
+            write(fd, message, strlen(message));
+            
+            currSeat = strtok(NULL, " ");
+        }
+        
+    } else {
+        strcat(message, error);
+    }
+    
 
-void getResponse(int timeout) {
+    
+    write(fd, message, strlen(message));
+    close(fd);
+    
+}
+
+void getResponse(int timeout, pid_t pid) {
     char  fifoName[10];
     int fdAnswers;
     char response[200];
     time_t base = time (0);
     
-    sprintf(fifoName, "ans%d", getpid());
+    sprintf(fifoName, "ans%d", pid);
     
     mkfifo(fifoName, 0660);
     fdAnswers = open(fifoName, O_RDONLY);
     
     while(readline(fdAnswers, response) && ((time(0) - base) < (timeout+2))) {
         printf("%s", response);
-        parseResponse(response);
+        parseResponse(response, pid);
     }
     
     close(fdAnswers);
     printf("ended timeout.");
 }
 
-void parseResponse(char *response) {
+void parseResponse(char *response, pid_t pid) {
     char *part;
     int id;
     int aux;
+    
     
     part = strtok (response, " ");
     id = atoi(part);
     
     if (id < 0) {
+//        writeLog(<#pid_t pid#>, <#int reservedSeats#>, <#char *seat#>, <#char *id#>)
         if (id == -1) {
             part = strtok(NULL, " ");
             aux = atoi(part);
@@ -125,6 +176,7 @@ void postRequest(char *argv[], pid_t pid) {
     int fdRequest;
     char message[200];
     int messagelen;
+    char *part;
     
     do {
         fdRequest = open("requests", O_WRONLY);
