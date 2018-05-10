4fb3776a2450af1aefd862d7d347b1e6400baa0e
Arthur26041996 <arthurmatta@hotmail.com>
2018-05-10 01:14:23 +0100

    request Handler - Validation

--- T2/server.c
+++ T2/server.c
@@ -18,28 +18,34 @@
 
 // Defined constants
 #define MAX_MSG_LEN 100
+#define MAX_CLI_SEATS 10
 
 // Global variables
-unsigned int num_room_seats;
-unsigned int num_tickets_offices;
-unsigned long int open_time;
+unsigned int num_room_seats;        // Number of seats
+unsigned int num_tickets_offices;   // Number of ticket offices (threads)
+unsigned long int open_time;        // ticket offices open time (seconds)
+unsigned int *seats;                // Seats
 
 typedef struct{
     unsigned int client;
-    unsigned int num_seats;
+    unsigned int num_wanted_seats;
     unsigned int *preferred_seats;
 } requests;
 
 // Functions
 int get_number_size(size_t number);
-char* makeClientFIFO(int pid);
-void requestHandler(char* request);
+char* getClientFIFO(int pid);
+requests* requestDisassembler(char* request);
+void *requestHandler(void *request);
 
+
+// MAIN
 int main(int argc, char const *argv[]) {
     int fd,         // File Descriptor
         fd_dummy,   // Dummy File Descriptor
         n;          // store the number of char's read
     char request[MAX_MSG_LEN+1];
+    requests *rq;
 
 
     // Usage
@@ -52,6 +58,7 @@ int main(int argc, char const *argv[]) {
     num_room_seats = strtol(argv[1], NULL, 10);
     num_tickets_offices = strtol(argv[2], NULL, 10);
     open_time = strtol(argv[3], NULL, 10);
+    memset(seats, 0, num_room_seats * sizeof(unsigned int));
 
     // Make FIFO 'ŕequests'
     if(mkfifo("requests", 0660) == 0){
@@ -77,7 +84,8 @@ int main(int argc, char const *argv[]) {
     // Receive requests
     while ((n = read(fd, request, MAX_MSG_LEN)) > 0) {
         printf("received request: %s\n", request);
-        requestHandler(request);
+        rq = requestDisassembler(request);
+
     }
 
     // Close FIFO 'requests'
@@ -94,45 +102,72 @@ int main(int argc, char const *argv[]) {
     exit(0);
 }
 
-void requestHandler(char* request){
-    requests rq;
+requests* requestDisassembler(char* request){
+    requests *rq = malloc(sizeof(requests));
     char *token,
          *delim = " ";
-    int *seats = NULL,
-        i = 0;
+    int i = 0;
 
     // get client PID
     printf("[REQUEST HANDLER]: extracting Client PID from request\n");
     token = strtok(request, delim);
-    rq.client = strtol(token, NULL, 10);
-    printf("[REQUEST HANDLER]: extracted Client PID from request: %d\n", rq.client);
+    rq->client = strtol(token, NULL, 10);
+    printf("[REQUEST HANDLER]: extracted Client PID from request: %d\n", rq->client);
 
     printf("[REQUEST HANDLER]: extracting Client desired number of seats from request\n");
     // get client desired number of seats
     token = strtok(NULL, delim);
-    rq.num_seats = strtol(token, NULL, 10);
-    printf("[REQUEST HANDLER]: extracted Client desired number of seats from request: %d\n", rq.num_seats);
+    rq->num_wanted_seats = strtol(token, NULL, 10);
+    printf("[REQUEST HANDLER]: extracted Client desired number of seats from request: %d\n", rq->num_wanted_seats);
 
     printf("[REQUEST HANDLER]: extracting Client desired seats from request\n");
     // get the desired seats
+    token = strtok(NULL, delim);
     while(token != NULL){
+        rq->preferred_seats = realloc(rq->preferred_seats, ++i * sizeof(int));
+        rq->preferred_seats[i-1] = strtol(token, NULL, 10);
         token = strtok(NULL, delim);
-        seats = (int*)realloc(seats, ++i * sizeof(int));
-        seats[i-1] = strtol(token, NULL, 10);
     }
-
-    rq.preferred_seats = memcpy(rq.preferred_seats, seats, i * sizeof(unsigned int));
     printf("[REQUEST HANDLER]: extracted Client desired number of seats from request\n");
 
-    printf("Cliente: %d\n", rq.client);
-    printf("Num seats: %d\n", rq.client);
+    printf("Cliente: %d\n", rq->client);
+    printf("Num seats: %d\n", rq->num_wanted_seats);
     printf("desired seats: ");
     for(int j = 0; j < i; j++){
-        printf("%d ", rq.preferred_seats[j]);
+        printf("%d ", rq->preferred_seats[j]);
     }
     printf("\n");
+
+    return rq;
 }
 
+void *requestHandler(void *request){
+    requests *rq = (requests*)request;
+
+    //validate request
+    if(rq->num_wanted_seats > MAX_CLI_SEATS){
+        fprintf(stderr, "[TICKET OFFICE %d]: Number of seats wanted higher than permited (%d)\n", pthread_self(), MAX_CLI_SEATS);
+        return NULL;
+    }
+    if((sizeof(rq->preferred_seats) / sizeof(unsigned int)) < rq->num_wanted_seats){
+        fprintf(stderr, "[TICKET OFFICE %d]: Number of preferred seats smaller than number of wanted seats (%d)\n", pthread_self(), rq->num_wanted_seats);
+        return NULL;
+    }
+    if((sizeof(rq->preferred_seats) / sizeof(unsigned int)) > MAX_CLI_SEATS){
+        fprintf(stderr, "[TICKET OFFICE %d]: Number of preferred seats bigger than permited (%d)\n", pthread_self(), MAX_CLI_SEATS);
+        return NULL;
+    }
+    for (int i = 0; i < (sizeof(rq->preferred_seats) / sizeof(unsigned int)); i++) {
+        if(rq->preferred_seats[i] > num_room_seats){
+            fprintf(stderr, "[TICKET OFFICE %d]: Preferred seat %d do not exist\n", pthread_self(), rq->preferred_seats[i]);
+            return NULL;
+        }
+    }
+
+    // Reservate seats
+
+    return NULL;
+}
 
 int get_number_size(size_t number){
     int i = 0;
@@ -143,7 +178,7 @@ int get_number_size(size_t number){
     return i;
 }
 
-char* makeClientFIFO(int pid){
+char* getClientFIFO(int pid){
     char* fifo = (char*)malloc(strlen("ans") + get_number_size(pid) + 1);
 
     sprintf(fifo, "ans%d", pid);
