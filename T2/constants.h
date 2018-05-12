63900ce020e8d971b4e2eb5235325921900d8773
Anderson Gralha <anderson.gralha@gmail.com>
2018-05-12 19:06:48 +0100

    updated constants

--- T2/constants.h
+++ T2/constants.h
@@ -1,6 +1,20 @@
 #ifndef constants_h_
 #define constants_h_
 
-#define MAX_CLI_SEATS
+// Defined constants
+#define MAX_MSG_LEN            100      // Maximum message lenght
+#define MAX_ROOM_SEATS        9999      // Maximum number of seats
+#define MAX_CLI_SEATS           99      // Maximum number of seats a client can book at a time
+#define RQT_FIFO_NAME    "requests"     // Requests FIFO name
+#define SV_LOG_NAME      "slog.txt"     // Server logging file name
+#define SV_BOOK_NAME    "sbook.txt"     // Server bookings file name
+
+// Defined Error constants
+#define MAX                    "-1"     // Number of desired seats is higher than permited
+#define NST                    "-2"     // Number of preferred seats is inval
+#define IID                    "-3"     // At least one preferred seat id is invalid
+#define ERR                    "-4"     // Any other parameter error
+#define NAV                    "-5"     // Number of desired seats not available
+#define FUL                    "-6"     // The room is full
 
 #endif 
--- T2/server.c
+++ T2/server.c
@@ -17,22 +17,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <regex.h>
-
-// Defined constants
-#define MAX_MSG_LEN            100      // Maximum message lenght
-#define MAX_ROOM_SEATS        9999      // Maximum number of seats
-#define MAX_CLI_SEATS           99      // Maximum number of seats a client can book at a time
-#define RQT_FIFO_NAME    "requests"     // Requests FIFO name
-#define SV_LOG_NAME      "slog.txt"     // Server logging file name
-#define SV_BOOK_NAME    "sbook.txt"     // Server bookings file name
-
-// Defined Error constants
-#define MAX                    "-1"     // Number of desired seats is higher than permited
-#define NST                    "-2"     // Number of preferred seats is inval
-#define IID                    "-3"     // At least one preferred seat id is invalid
-#define ERR                    "-4"     // Any other parameter error
-#define NAV                    "-5"     // Number of desired seats not available
-#define FUL                    "-6"     // The room is full
+#include "constants.h"
 
 // Defined function-like macros
 #define DELAY(usec)     usleep(usec)    // Simulate the existence of some delay
